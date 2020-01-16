af09d2066d757c574479eb317beee936cae92ee4
Daniel Colman <danrcolman@gmail.com>
2020-01-15 19:43:45 -0800

    Upload from computer

---
+++ Assets/LookAtTarget.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class LookAtTarget : MonoBehaviour
+{
+    GameObject target;
+    bool rotateToFaceMovingTarget = false;
+
+    public GameObject Target { get => target; set => target = value; }
+    public bool RotateToFaceMovingTarget { get => rotateToFaceMovingTarget; set => rotateToFaceMovingTarget = value; }
+
+    private void Awake()
+    {
+        //Sets rotation target initially to main camera
+        Target = Camera.main.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Target.transform);
+    }
+
+    public void SetTarget(GameObject go)
+    {
+        Target = go;
+    }
+}
---
+++ Assets/StarfieldGenerator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class StarfieldGenerator : MonoBehaviour
+{
+    [Tooltip("Set in Inspector")]
+    [SerializeField] private GameObject star;
+    [SerializeField] private int numberOfStars;
+    [SerializeField] private float minDistance = 1;
+    [SerializeField] private float maxDistance = 1000;
+    [SerializeField] private bool rotateToFacePlayer = true;
+
+    private GameObject player;
+    GameObject tempInstantiatedObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = Camera.main.gameObject;
+        for (int i = 0; i < numberOfStars; i++)
+        {
+            InstantiateRandomObject(star);
+        }
+
+    }
+
+    void InstantiateRandomObject(GameObject go)
+    {
+        Vector3 randomPos = player.transform.position + 
+                            (Random.onUnitSphere * Random.Range(minDistance, maxDistance));
+        tempInstantiatedObject = Instantiate(go, randomPos, Quaternion.identity); 
+        if(rotateToFacePlayer)
+        {
+            LookAtTarget(tempInstantiatedObject, player);
+        }
+    }
+
+    // make a looker rotate to face a target
+    void LookAtTarget(GameObject looker, GameObject target)
+    {
+        looker.transform.LookAt(target.transform);
+    }
+}
